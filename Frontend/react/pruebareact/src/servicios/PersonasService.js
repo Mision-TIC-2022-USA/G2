93f14521edb846ec7b99c62f0aeffff076f23d95
Angel Morales <angelmoralesb@gmail.com>
2021-12-15 14:24:51 -0500

    Docker

--- Frontend/react/pruebareact/src/servicios/PersonasService.js
+++ Frontend/react/pruebareact/src/servicios/PersonasService.js
@@ -1,8 +1,12 @@
 
 import axios from 'axios';
+//modificar la ip para produccion
+//en mi caso 140.238.180.131
+const ip = "localhost:8080";
 
-const PERSONAS_API_BASE_URL = "http://localhost:8080/api/persona";
+const PERSONAS_API_BASE_URL = "http://"+ip;
 
 export function getPersonas() {
-    return axios.get(`${PERSONAS_API_BASE_URL}/all`);
+  console.log(PERSONAS_API_BASE_URL);
+    return axios.get(`${PERSONAS_API_BASE_URL}/api/persona/all`);
   }
